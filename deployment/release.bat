1129ceb5d3db68d38da479ef79d50613ad81087a
Nolan Pather <nolan@entelect.co.za>
2015-01-13 15:05:53 +0200

    please work

--- deployment/release.bat
+++ deployment/release.bat
@@ -1,13 +1,19 @@
 echo off
+set "dataPath=%~1"
+
 echo "--------------------------------------------------------------------------------"
 echo "Commit changes"
 for /f "delims=" %%a in ('version.exe package.json') do @set version=%%a
 echo New version = %version%
-git pull origin master
+
+# Run git commands as the SSH identity provided by the keyfile ~/.ssh/admin
+git config alias.admin \!"git-as.sh %dataPath%/GitareHero"
+
+echo "commit version change"
 git commit -a -m "Incremented version to %version% for release"
 
 echo "--------------------------------------------------------------------------------"
 echo "Tag this release and push to origin"
-git tag %version%
-git push origin master --tags
+git admin tag %version%
+git admin push origin master --tags
 echo "--------------------------------------------------------------------------------"
