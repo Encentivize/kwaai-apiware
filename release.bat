cc7be911478e20173e9936fce8ba8cdaaf4e17ac
Nolan Pather <nolan@entelect.co.za>
2015-01-13 16:22:15 +0200

    getting rid of broken scripts

--- release.bat
+++ release.bat
@@ -1,17 +1,12 @@
 echo off
-
 echo "--------------------------------------------------------------------------------"
 echo "Commit changes"
-dir
-for /f "delims=" %%a in (deployment\\version.exe package.json) do @set version=%%a
+for /f "delims=" %%a in ('version.exe package.json') do @set version=%%a
 echo New version = %version%
-
-git config alias.admin "deployment/git-as.sh %env.TEAMCITY_DATA_PATH%/GitareHero"
-
-echo "commit version change"
 git commit -a -m "Incremented version to %version% for release"
 
 echo "--------------------------------------------------------------------------------"
 echo "Tag this release and push to origin"
-git admin tag %version%
-git admin push origin master --tags
+git tag %version%
+git push origin master --tags
+echo "--------------------------------------------------------------------------------"
