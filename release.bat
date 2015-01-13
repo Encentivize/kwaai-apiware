506544b19345b3742b460211cb7af8f581e0de73
Nolan Pather <nolan@entelect.co.za>
2015-01-13 13:16:37 +0200

    pull master for auth test

--- release.bat
+++ release.bat
@@ -3,6 +3,7 @@ echo "--------------------------------------------------------------------------
 echo "Commit changes"
 for /f "delims=" %%a in ('version.exe package.json') do @set version=%%a
 echo New version = %version%
+git pull origin master
 git commit -a -m "Incremented version to %version% for release"
 
 echo "--------------------------------------------------------------------------------"
