06998a766d341b5b7c17ea94520e9d6622bc2860
Nolan Pather <nolan@entelect.co.za>
2015-01-13 15:39:52 +0200

    removed slash

--- deployment/release.bat
+++ deployment/release.bat
@@ -2,7 +2,7 @@ echo off
 
 echo "--------------------------------------------------------------------------------"
 echo "Commit changes"
-for /f "delims=" %%a in ('/version.exe ../package.json') do @set version=%%a
+for /f "delims=" %%a in ('version.exe ../package.json') do @set version=%%a
 echo New version = %version%
 
 git config alias.admin \!"git-as.sh %env.TEAMCITY_DATA_PATH%/GitareHero"
